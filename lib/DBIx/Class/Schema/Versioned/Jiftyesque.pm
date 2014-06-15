ccbbe560d8dd597c2f65425ae750591ef08a74f9
Peter Mottram <peter@sysnix.com>
2014-06-15 20:15:09 +0200

    initial upgrade code working but need better tests

--- lib/DBIx/Class/Schema/Versioned/Jiftyesque.pm
+++ lib/DBIx/Class/Schema/Versioned/Jiftyesque.pm
@@ -125,6 +125,8 @@ use base 'DBIx::Class::Schema::Versioned';
 
 use Carp;
 use Data::Dumper::Concise;
+use SQL::Translator;
+use SQL::Translator::Diff;
 use version 0.77;
 
 our @schema_versions;
@@ -135,28 +137,13 @@ Many methods are inherited or overloaded from L<DBIx::Class::Schema::Versioned>.
 
 =head2 connection
 
-Overloaded method. This checks the DBIC schema version against the DB version and warns if they are not the same or if the DB is unversioned.
-
-To avoid the checks on connect, set the environment var DBIC_NO_VERSION_CHECK or alternatively you can set the ignore_version attr in the forth argument like so:
-
-  my $schema = MyApp::Schema->connect(
-    $dsn,
-    $user,
-    $password,
-    { ignore_version => 1 },
-  );
+Overloaded method. This checks the DBIC schema version against the DB version and ???? TODO ???? (undecided so far) if they are not the same or if the DB is unversioned.
 
 =cut
 
 sub connection {
-  my $self = shift;
-  $self->next::method(@_);
-  $self->_on_connect();
-  return $self;
-}
-
-sub _on_connect {
     my $self = shift;
+    $self->next::method(@_);
 
     my $conn_info = $self->storage->connect_info;
     $self->{vschema} = DBIx::Class::Version->connect(@$conn_info);
@@ -164,9 +151,10 @@ sub _on_connect {
 
     my $vtable = $self->{vschema}->resultset('Table');
 
-    my $version = $self->get_db_version();
+    my $version = $conn_attrs->{_version} || $self->get_db_version();
 
-    unless ( $version ) {
+    unless ($version) {
+
         # TODO: checks for unversioned
         # - do we throw exception?
         # - do we install automatically?
@@ -175,9 +163,9 @@ sub _on_connect {
         $version = $self->schema_version;
     }
 
-    $self->_cleanup_versions( $version );
+    $self->versioned_schema( $version, $conn_attrs->{_type} );
 
-    return 1;
+    return $self;
 }
 
 =head2 deploy
@@ -213,107 +201,6 @@ sub ordered_schema_versions {
     };
 }
 
-=head2 _cleanup_versions
-
-Used to weed out classes and columns that are not appropriate for our current database version based on since/until values. Collects available versions at the same time.
-
-=cut
-
-sub _cleanup_versions {
-    my ( $self, $_version ) = @_;
-
-    my $pversion = version->parse( $_version );
-
-    #warn "DB VERSION " . $self->get_db_version;
-
-    foreach my $source_name ( $self->sources ) {
-
-        #warn "SOURCE NAME $source_name";
-
-        my $source = $self->source($source_name);
-
-        # check columns before deciding on class-level since/until to make sure
-        # we don't miss any versions
-
-        foreach my $column ( $source->columns ) {
-
-            my $extra = $source->column_info($column)->{extra};
-
-            my $since = $extra->{since};
-            my $until = $extra->{until};
-
-            my $name = "$source_name column $column";
-            my $sub  = sub {
-                my $source = shift;
-                $source->remove_column($column);
-            };
-            $self->_since_until( $pversion, $extra->{since}, $extra->{until}, $name, $sub, $source );
-        }
-
-        # now check relations
-
-        foreach my $relation_name ( $source->relationships ) {
-
-            my $attrs = $source->relationship_info( $relation_name )->{attrs};
-
-            next unless defined $attrs;
-
-            my $extra = $attrs->{extra};
-
-            next unless defined $extra;
-
-            my $since = $extra->{since};
-            my $until = $extra->{until};
-
-            my $name = "$source_name relationship $relation_name";
-            my $sub  = sub {
-                my $source = shift;
-                my %rels = %{ $source->_relationships };
-                delete $rels{$relation_name};
-                $source->_relationships(\%rels);
-            };
-            $self->_since_until( $pversion, $extra->{since}, $extra->{until}, $name, $sub, $source );
-        }
-
-        # now check class-level since/until
- 
-        my ( $since, $until );
-
-        my $extra = $source->resultset_attributes->{extra};
-
-        if ( defined $extra ) {
-            $since = $extra->{since} if defined $extra->{since};
-            $until = $extra->{until} if defined $extra->{until};
-        }
-
-        my $name = $source_name;
-        my $sub  = sub {
-            my $class = shift;
-            $class->unregister_source($source_name);
-        };
-        $self->_since_until( $pversion, $extra->{since}, $extra->{until}, $name, $sub, $self );
-    }
-}
-
-sub _since_until {
-    my ( $self, $pversion, $since, $until, $name, $sub, $thing ) = @_;
-
-    if ( $since && $until && ( version->parse($since) > version->parse($until) ) ) {
-        $self->throw_exception("$name has since greater than until");
-    }
-
-    push( @schema_versions, $since ) if $since;
-    push( @schema_versions, $until ) if $until;
-
-    # until is absolute so parse before since
-    if ( $until && $pversion > version->parse($until) ) {
-        $sub->($thing);
-    }
-    if ( $since && $pversion < version->parse($since) ) {
-        $sub->($thing);
-    }
-}
-
 =head2 upgrade
 
 Inherited method. Call this to attempt to upgrade your database from the version it is at to the version this DBIC schema is at. If they are the same it does nothing.
@@ -345,6 +232,203 @@ sub upgrade_single_step {
         return;
     }
     carp "attempting upgrade from $db_version to $target_version";
+
+    my $sqlt_type = $self->storage->sqlt_type;
+
+    # translate current schema
+
+    my $curr_tr = SQL::Translator->new(
+        parser   => 'SQL::Translator::Parser::DBIx::Class',
+        parser_args => {
+            dbic_schema => $self,
+        },
+        producer => $sqlt_type,
+    ) or $self->throw_exception(SQL::Translator->error);
+    $curr_tr->translate;
+
+    # since schema contains new things to be added
+
+    my $since_schema =
+      ref($self)
+      ->connect( "DBI:Mock:", undef, undef,
+        { _version => $target_version, _type => 'since' } );
+
+    my $since_tr = SQL::Translator->new(
+        parser   => 'SQL::Translator::Parser::DBIx::Class',
+        parser_args => {
+            dbic_schema => $since_schema,
+        },
+        producer => $sqlt_type,
+    ) or $self->throw_exception(SQL::Translator->error);
+    $since_tr->translate;
+
+    # until schema contains things to be removed
+
+    my $until_schema =
+      ref($self)
+      ->connect( "DBI:Mock:", undef, undef,
+        { _version => $target_version, _type => 'until' } );
+
+    my $until_tr = SQL::Translator->new(
+        parser   => 'SQL::Translator::Parser::DBIx::Class',
+        parser_args => {
+            dbic_schema => $until_schema,
+        },
+        producer => $sqlt_type,
+    ) or $self->throw_exception(SQL::Translator->error);
+    $until_tr->translate;
+
+    my $since_diff = SQL::Translator::Diff->new({
+        output_db     => $sqlt_type,
+        source_schema => $curr_tr->schema,
+        target_schema => $since_tr->schema,
+        ignore_index_names => 1,
+        ignore_constraint_names => 1,
+    })->compute_differences->produce_diff_sql;
+
+    $since_diff =~ s/\n(BEGIN|COMMIT);\n//g;
+
+    my $until_diff = SQL::Translator::Diff->new({
+        output_db     => $sqlt_type,
+        source_schema => $since_tr->schema,
+        target_schema => $until_tr->schema,
+        ignore_index_names => 1,
+        ignore_constraint_names => 1,
+    })->compute_differences->produce_diff_sql;
+
+    $until_diff =~ s/\n(BEGIN|COMMIT);\n//g;
+
+    $self->storage->dbh_do(
+        sub {
+            my ( $storage, $dbh, $diff ) = @_;
+            $dbh->do($diff);
+        },
+        $since_diff
+    );
+    $self->storage->dbh_do(
+        sub {
+            my ( $storage, $dbh, $diff ) = @_;
+            $dbh->do($diff);
+        },
+        $until_diff
+    );
+
+    # set row in dbix_class_schema_versions table
+    $self->_set_db_version({version => $target_version});
+}
+
+=head2 versioned_schema
+
+=item Arguments: version - the schema version we want to deploy
+
+=item Arguments: type - one of since/until
+
+Parse schema and remove classes, columns and relationships that are not valid for the requested version. The C<type> should be undef for use during deploy but set to since or until as part of schema diff generation during upgrade.
+
+=cut
+
+sub versioned_schema {
+    my ( $self, $_version, $type ) = @_;
+
+    my $pversion = version->parse($_version);
+
+    foreach my $source_name ( $self->sources ) {
+
+        my $source = $self->source($source_name);
+
+        # check columns before deciding on class-level since/until to make sure
+        # we don't miss any versions
+
+        foreach my $column ( $source->columns ) {
+
+            my $extra = $source->column_info($column)->{extra};
+
+            my $since = $extra->{since};
+            my $until = $extra->{until};
+
+            my $name = "$source_name column $column";
+            my $sub  = sub {
+                my $source = shift;
+                $source->remove_column($column);
+            };
+            $self->_since_until( $pversion, $type, $extra->{since},
+                $extra->{until}, $name, $sub, $source );
+        }
+
+        # now check relations
+
+        foreach my $relation_name ( $source->relationships ) {
+
+            my $attrs = $source->relationship_info($relation_name)->{attrs};
+
+            next unless defined $attrs;
+
+            my $extra = $attrs->{extra};
+
+            next unless defined $extra;
+
+            my $since = $extra->{since};
+            my $until = $extra->{until};
+
+            my $name = "$source_name relationship $relation_name";
+            my $sub  = sub {
+                my $source = shift;
+                my %rels   = %{ $source->_relationships };
+                delete $rels{$relation_name};
+                $source->_relationships( \%rels );
+            };
+            $self->_since_until( $pversion, $type, $extra->{since},
+                $extra->{until}, $name, $sub, $source );
+        }
+
+        # now check class-level since/until
+
+        my ( $since, $until );
+
+        my $extra = $source->resultset_attributes->{extra};
+
+        if ( defined $extra ) {
+            $since = $extra->{since} if defined $extra->{since};
+            $until = $extra->{until} if defined $extra->{until};
+        }
+
+        my $name = $source_name;
+        my $sub  = sub {
+            my $class = shift;
+            $class->unregister_source($source_name);
+        };
+        $self->_since_until( $pversion, $type, $extra->{since}, $extra->{until},
+            $name, $sub, $self );
+    }
+}
+
+sub _since_until {
+    my ( $self, $pversion, $type, $since, $until, $name, $sub, $thing ) = @_;
+
+    if (   $since
+        && $until
+        && ( version->parse($since) > version->parse($until) ) )
+    {
+        $self->throw_exception("$name has since greater than until");
+    }
+
+    # only take note of versions if we are called with undef $type
+    unless ($type) {
+        push( @schema_versions, $since ) if $since;
+        push( @schema_versions, $until ) if $until;
+    }
+
+    # until is absolute so parse before since
+    if (   ( !$type || ($type eq 'until') )
+        && $until
+        && $pversion > version->parse($until) )
+    {
+        $sub->($thing);
+    }
+    if ( $since && $pversion < version->parse($since) )
+    {
+        $sub->($thing);
+    }
 }
 
 =head1 CAVEATS
--- t/upgrade.t
+++ t/upgrade.t
@@ -6,6 +6,7 @@ use warnings FATAL => 'all';
 use Test::Most;
 
 use Class::Unload;
+use Data::Dumper::Concise;
 use File::Spec;
 use File::Temp;
 use version 0.77;
@@ -34,11 +35,15 @@ VERSION_0_001: {
     use_ok 'TestVersion_v0_001';
 
     $schema = TestVersion::Schema->connect("dbi:SQLite:dbname=$dbfile");
+
+    # deploy (also installs initial version)
+
     lives_ok( sub { $schema->deploy }, "deploy schema" );
 
     cmp_ok( $schema->schema_version, 'eq', '0.001', "Check schema version" );
     cmp_ok( $schema->get_db_version, 'eq', '0.001', "Check db version" );
 
+    Class::Unload->unload('TestVersion::Schema');
     Class::Unload->unload('TestVersion::Foo');
     Class::Unload->unload('TestVersion::Bar');
 }
@@ -56,6 +61,30 @@ VERSION_0_002: {
 
     # let's upgrade!
 
+    lives_ok( sub { $schema->upgrade }, "Upgrade " . $schema->get_db_version . " to " . $schema->schema_version );
+
+    cmp_ok( $schema->get_db_version, 'eq', '0.002', "Check db version post upgrade" );
+
+    my $dbh = $schema->storage->dbh;
+
+    cmp_deeply( [ sort $dbh->tables ], [qw(bars foos)], "Check tables" );
+    cmp_deeply( [ sort $schema->sources ], [qw(Bar)], "Bar only" );
+
+    # columns
+    my $foo = $schema->source('Foo');
+    cmp_deeply(
+        [ sort $foo->columns ],
+        [qw(age bars_id foos_id height)],
+        "Foo columns OK"
+    );
+    my $bar = $schema->source('Bar');
+    cmp_deeply(
+        [ sort $bar->columns ],
+        [qw(age bars_id weight)],
+        "Bar columns OK"
+    );
+
+    Class::Unload->unload('TestVersion::Schema');
     Class::Unload->unload('TestVersion::Foo');
     Class::Unload->unload('TestVersion::Bar');
 }
@@ -63,43 +92,26 @@ VERSION_0_002: {
 done_testing;
 exit;
 
-
 VERSION_0_003: {
 
     use_ok 'TestVersion_v0_003';
 
-    $schema = TestVersion::Schema->connect("dbi:SQLite:dbname=:memory:");
-
-    @versions = ( '0.001', '0.002', '0.003', '0.004', '0.005', '0.3' );
+    $schema = TestVersion::Schema->connect("dbi:SQLite:dbname=$dbfile");
 
     cmp_ok( $schema->schema_version, 'eq', '0.003', "Check schema version" );
-    cmp_ok( $schema->get_db_version, '==', 0, "db version not defined yet" );
+    cmp_ok( $schema->get_db_version, 'eq', '0.002', "Check db version" );
 
-    lives_ok( sub { $schema->deploy }, "deploy schema" );
-    cmp_ok( $schema->get_db_version, 'eq', '0.003', "Check db version" );
+    # let's upgrade!
 
-    cmp_deeply( [ $schema->ordered_schema_versions ],
-        \@versions, "Check we found all expected versions" )
-      || (diag "got: "
-        . join( " ", $schema->ordered_schema_versions )
-        . "\nexpect: "
-        . join( " ", @versions ) );
+    lives_ok( sub { $schema->upgrade }, "Upgrade " . $schema->get_db_version . " to " . $schema->schema_version );
 
-    # tables
-
-    cmp_deeply( [ sort $schema->sources ], [qw(Bar)], "Bar only" );
-
-    # columns
-    my $bar = $schema->source('Bar');
-    cmp_deeply(
-        [ sort $bar->columns ],
-        [qw(age bars_id height weight)],
-        "Bar columns OK"
-    );
-
+    Class::Unload->unload('TestVersion::Schema');
     Class::Unload->unload('TestVersion::Foo');
     Class::Unload->unload('TestVersion::Bar');
 }
+
+done_testing;
+exit;
 
 VERSION_0_3: {
 
